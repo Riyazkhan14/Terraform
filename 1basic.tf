95156170598ea706ca9ba797fcab17c07094c4ec
Riyazuddin <21050074+Riyazkhan14@users.noreply.github.com>
2021-05-07 21:22:37 +0530

    Update 1basic.tf

--- 1basic.tf
+++ 1basic.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIAUYW74ABSYNNMVKEJ"
-secret_key = "NjcmzRdwmWw4odZ+LBTfmb+nrjxFKtkk9sk8LLee"
+access_key = "your_access_key"
+secret_key = "your_secret_key"
 }
 
 resource "aws_instance" "OS1byTF" {
